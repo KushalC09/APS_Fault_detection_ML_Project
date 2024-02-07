bf9fa2bd4bc43a0a903a22eee1315dc119ac3c3c
KushalC09 <KushalC09@github.com>
2024-02-07 20:15:11 +0530

    setup structure done

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+setup(
+
+     name = "sensor",
+     version="0.0.1",
+     author="Kushal",
+     author_email="kchaple9@gmail.com",
+     packages = find_packages(),
+     install_requires=get_requirements(),
+
+)
